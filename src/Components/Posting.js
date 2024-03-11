b6033951b62a8c7f2908e86367df484820776443
M4RKL4B0Y <markvlaboy@gmail.com>
2024-03-10 23:29:37 -0400

    final commit

--- src/Components/Posting.js
+++ src/Components/Posting.js
@@ -3,7 +3,10 @@ const Posting = (props) => {
         <div className="posting">
             <h1>{props.posting.title}</h1>
             <p>{props.posting.description}</p>
+            
             <img src={props.posting.imageURL} alt={props.posting.title} />
+
+            <p>{props.posting.price}</p>
         </div>
     )
 }
